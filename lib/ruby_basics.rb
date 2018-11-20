e8614e3a7b62dc895989a7aff5f732e6d0594412
synchronous-environment-3669 <kyu22@buffalo.edu>
2018-11-20 00:46:49 +0000

    automatically backed up by learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,33 @@
 def division(num1, num2)
- 
+  quotient = num1 / num2
+  return quotient
 end
 
 def assign_variable(value)
+  name = value
+  return name
   
 end
 
 def argue
-  
+  return argue
 end
 
-def greeting
+def greeting(greeting, name)
+  
 
 end
 
 def return_a_value
+  puts "Nice"
 
 end
 
 def last_evaluated_value
-
+  puts "expert"
 end
 
-def pizza_party
+def pizza_party(toppings = "cheese")
+  return toppings
 
 end
